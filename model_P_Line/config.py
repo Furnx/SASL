d6ab1c6876589338c96d41f17a0d7e6598c6b9c1
Tumo <tumogjhb025@student.wethinkcode.co.za>
2025-12-19 18:53:43 +0200

    phase 7

--- model_P_Line/config.py
+++ model_P_Line/config.py
@@ -140,7 +140,7 @@ VOCAB_SCHEDULE = {
 
 # !!! CHANGE THIS VARIABLE TO WORK ON A SPECIFIC WEEK !!!
 # Options: 'Week_1_Greetings', 'Week_2_Manners', 'All', etc.
-ACTIVE_WEEK = 'Week_1_Greetings'
+ACTIVE_WEEK = 'Week_11_Actions'
 
 def get_actions():
     """
